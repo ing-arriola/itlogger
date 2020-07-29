f06aca2203aebbacb37d80aa86ec7f679e7f824a
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-28 20:06:51 -0600

    corrected a mistake on navbar and also added the logs component

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,19 @@
+import React,{useState,useEffect} from 'react'
+
+const Logs = () => {
+    const [Logs,setLogs]=useState([])
+    const [loading,setLoading]=useState(false)
+
+    const getLogs = async () => {
+        setLoading(true)
+        
+    }
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Logs
