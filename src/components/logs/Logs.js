cef2366c41002ab8c4ea212ef054777b3a27f477
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-28 20:10:51 -0600

    Added a function getLogs on Logs.js and a hook to call it

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -4,9 +4,22 @@ const Logs = () => {
     const [Logs,setLogs]=useState([])
     const [loading,setLoading]=useState(false)
 
+    useEffect(() => {
+        getLogs()
+        //eslint-disable-next-line
+    })
+
     const getLogs = async () => {
         setLoading(true)
-        
+        const res=await fetch('/logs')
+        const data = await res.json()
+
+        setLogs(data)
+        setLoading(false)
+    }
+
+    if(loading){
+        return <h4>Loading...</h4>
     }
 
     return (
