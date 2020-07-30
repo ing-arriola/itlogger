d9574c59a243bfdc8600cba38e88cf46c1cde8cd
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-29 19:33:56 -0600

    AddBtn now added to UI also Added first part of AddLogModal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React,{useEffect,Fragment} from 'react';
-import SearchBar from './components/layout/SearchBar'
-import Logs from './components/logs/Logs'
+import React, { useEffect, Fragment } from "react";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
 
-import 'materialize-css/dist/css/materialize.min.css'
-import M from 'materialize-css/dist/js/materialize.min.js'
-import './App.css';
-
-
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
 
 const App = () => {
-  useEffect(()=>{
+  useEffect(() => {
     //init materialize JS
-    M.AutoInit()
-  })
+    M.AutoInit();
+  });
   return (
     <Fragment>
-      <SearchBar/>
-      <div className='container'>
-        <Logs/>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <Logs />
       </div>
     </Fragment>
   );
-}
+};
 
 export default App;
