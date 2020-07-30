6ed8abc29103f2e024e5549d5a792acf08ddd04b
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-29 20:08:52 -0600

    completed  AddLogModal and styles for enter button has been changed

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
-
-const onSubmit = () => {
-  console.log("HELLO");
-};
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const AddLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
+
+  const onSubmit = () => {
+    if (message === "" || tech === "") {
+      M.toast({ html: "Please enter a message and tech" });
+    } else {
+      console.log(message, tech, attention);
+    }
+  };
+
   return (
     <div id="add-log-modal" className="modal" style={modalStyle}>
       <div className="modal-content">
@@ -66,7 +72,7 @@ const AddLogModal = () => {
         <a
           href="#!"
           onClick={onSubmit}
-          className="modal-close waves-effect waves-green btn-flat"
+          className="modal-close waves-effect blue waves-green btn"
         >
           Enter
         </a>
