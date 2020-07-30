bd2633a49ec6cd9e111ff656f93f93c868b5f1b0
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-29 20:00:06 -0600

    almost done with AddLogModal

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
+const onSubmit = () => {
+  console.log("HELLO");
+};
+
 const AddLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
@@ -21,6 +25,51 @@ const AddLogModal = () => {
             </label>
           </div>
         </div>
+
+        <div className="row">
+          <div className="input-field">
+            <select
+              name="tech"
+              value={tech}
+              className="browser-default"
+              onChange={(e) => setTech(e.target.value)}
+            >
+              <option value="" disabled>
+                Select Technician
+              </option>
+              <option value="John">John</option>
+              <option value="Sam">Sam</option>
+              <option value="Raul">Raul</option>
+              <option value="Sara">Sara</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field">
+            <p>
+              <label>
+                <input
+                  type="checkbox"
+                  className="filled-in"
+                  checked={attention}
+                  value={attention}
+                  onChange={(e) => setAttention(!attention)}
+                />
+                <span>Needs attention</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect waves-green btn-flat"
+        >
+          Enter
+        </a>
       </div>
     </div>
   );
