716a1d333860ea1c65d1c020d9917ebfa16153a1
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-01 13:55:50 -0600

    Added TechSelectOptions and included into EditLogModal and AddLogModal to replace the hardcoding names of technicians

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TechSelectOptions from "../techs/TechSelectOptions";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { addLog } from "../../actions/logActions";
@@ -58,10 +59,7 @@ const AddLogModal = ({ addLog }) => {
               <option value="" disabled>
                 Select Technician
               </option>
-              <option value="John">John</option>
-              <option value="Sam">Sam</option>
-              <option value="Raul">Raul</option>
-              <option value="Sara">Sara</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import TechSelectOptions from "../techs/TechSelectOptions";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { updateLog } from "../../actions/logActions";
@@ -64,10 +65,7 @@ const EditLogModal = ({ current, updateLog }) => {
               <option value="" disabled>
                 Select Technician
               </option>
-              <option value="John">John</option>
-              <option value="Sam">Sam</option>
-              <option value="Raul">Raul</option>
-              <option value="Sara">Sara</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
