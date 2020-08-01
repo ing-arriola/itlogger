567fb0220184abb52cd66b455804a1ab62174c1d
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-01 06:43:29 -0600

    updateLog added into reducer and LogItem

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -72,16 +72,17 @@ export const deleteLog = (id) => async (dispatch) => {
 
 //UPDATE
 
-export const updateLog = (id) => async (dispatch) => {
+export const updateLog = (log) => async (dispatch) => {
   try {
     setLoading();
-    await fetch(`/logs/${id}`, {
+    const res = await fetch(`/logs/${log.id}`, {
       method: "PUT",
       body: JSON.stringify(log),
       headers: {
         "Content-type": "application/json",
       },
     });
+    const data = await res.json();
 
     dispatch({
       type: UPDATE_LOG,
