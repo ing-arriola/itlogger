ba6f41580192f39ecbf9948e83b04aadf8501b0d
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-01 09:38:41 -0600

    Added techActions and techReducer, also fixed an error on logActions (on catch the payload on error now is err.response.statusText which actually give the error text)

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -22,7 +22,7 @@ export const getLogs = () => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: LOGS_ERRORS,
-      payload: err.response.data,
+      payload: err.response.statusText,
     });
   }
 };
@@ -46,7 +46,7 @@ export const addLog = (log) => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: LOGS_ERRORS,
-      payload: err.response.data,
+      payload: err.response.statusText,
     });
   }
 };
@@ -66,7 +66,7 @@ export const deleteLog = (id) => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: LOGS_ERRORS,
-      payload: err.response.data,
+      payload: err.response.statusText,
     });
   }
 };
@@ -92,7 +92,7 @@ export const updateLog = (log) => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: LOGS_ERRORS,
-      payload: err.response.data,
+      payload: err.response.statusText,
     });
   }
 };
@@ -109,7 +109,7 @@ export const searchLogs = (text) => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: LOGS_ERRORS,
-      payload: err.response.data,
+      payload: err.response.statusText,
     });
   }
 };
